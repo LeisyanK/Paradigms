96e9a882deced6dd5e622a29d375d2a43b7458f0
Kashapova Leisyan <LeisyanRA@mail.ru>
2023-09-15 18:41:55 +0500

    hw seminar1

--- seminar1/homework.py
+++ seminar1/homework.py
@@ -3,18 +3,15 @@ def Partition(array, l, r):
     print("----")
     x = array[r]  # опорный элемент
     begin = l
-    for i in range(r+1):
+    for i in range(l, r):
         if(array[i] <= x):
             tmp = array[begin]
             array[begin] = array[i]
             array[i] = tmp
-            for num in array:
-                print(num, end=" ")
-            print()
+            # for num in array:
+            #     print(num, end=" ")
+            # print()
             begin = begin + 1
-        
-        # print(begin)
-    begin = begin - 1
     array[r] = array[begin]
     array[begin] = x
     return begin
@@ -28,9 +25,9 @@ def QuickSort(array, l ,r):
 
 array = [6, 4, 9, 3, 2, 9, 1, 7]
 # declarative
-# array2 = sorted(array)
-# for num in array2:
-#     print(num)
+array2 = sorted(array)
+for num in array2:
+    print(num)
 
 # imperative
 size = len(array)
